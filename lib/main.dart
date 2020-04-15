cf2d42d56b9950537d8e190f887afb6a8c395c26
ASUS® <irshika5sahanmali@gmail.com>
2020-04-15 11:43:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tree_app/screens/feed.dart';
+import 'package:tree_app/screens/login.dart';
+
+import 'notifier/auth_notifier.dart';
+
+void main() => runApp(
+  MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => AuthNotifier(),
+    )
+  ],
+  ));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tree App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blue,
+      ),
+      home: Consumer<AuthNotifier>(
+        builder: (context, notifier, child){
+          return notifier.user != null? Feed() : Login();
+        },
+      ),
+    );
+  }
+}
